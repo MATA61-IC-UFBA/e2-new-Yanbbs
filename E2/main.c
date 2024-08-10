4e810f0d2bd7bea80e3e9d42c0566a10e3e71015
YanBrandao1 <Yanbbs00@gmail.com>
2024-08-10 17:55:11 -0300

    Copiando a atividade E2 feita anteriormente para a pasta E2-New

--- E2/main.c
+++ E2/main.c
@@ -1,2 +1,27 @@
 /* main.c */
 
+
+#include <stdio.h>
+#include "token.h"
+
+extern int yylex(); // returns the number of the token
+extern char *yytext; // returns the lexeme of the input
+
+int main (){
+
+	token_t token = -1;
+	while (token != EOL) {
+		token = yylex();
+		if (token == ERROR){
+			printf("erro léxico: %s \n",yytext);
+			}
+		else if (token == NUMBER){
+			printf("constante decimal: %s \n", yytext);
+			}
+		else{
+			printf("código do token: %d \n",token);
+			}
+		}
+
+}
+
--- E2/token.h
+++ E2/token.h
@@ -1,1 +1,13 @@
 /* token.h */
+
+
+
+typedef enum {
+	EOL=0,	// 0
+	NUMBER, // 1
+	PLUS,	// 2
+	MINUS,	// 3
+	TIMES,	// 4
+	DIV,	// 5
+	ERROR,	// 6
+} token_t;
